9f2ee0885505fda74dfc07dd361068d50820e86c
Banerry <43846981+Banerry@users.noreply.github.com>
2019-11-03 17:10:37 +0100

    added sirens

---
+++ NeuralNetworkRaceGame2/Assets/Scripts/CarLights.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarLights : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Light red;
+    public Light blue;
+
+    public float maxInstensity;
+    public float speed;
+
+    private float trueMaxIntensity;
+    private bool resume = false;
+
+    void Start()
+    {
+        trueMaxIntensity = maxInstensity / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkToContinue();
+        siren();
+    }
+
+    public void siren()
+    {
+        if (resume)
+        {
+        Debug.Log(Time.time);
+        red.intensity = trueMaxIntensity * Mathf.Cos(Time.time * speed) + trueMaxIntensity;
+        blue.intensity = -trueMaxIntensity * Mathf.Cos(Time.time * speed) + trueMaxIntensity;
+        }
+        else
+        {
+            red.intensity = 0;
+            blue.intensity = 0;
+        }
+    }
+    public void checkToContinue()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            resume = !resume;
+        }
+    }
+}
---
+++ NeuralNetworkRaceGame2/Assets/Scripts/turningSiren.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turningSiren : MonoBehaviour
+{
+    public float speed;
+    public Light red;
+    public Light blue;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        red.transform.Rotate(Vector3.up * speed * Time.deltaTime, Space.World);
+        blue.transform.Rotate(Vector3.up * speed * Time.deltaTime, Space.World);
+    }
+}
