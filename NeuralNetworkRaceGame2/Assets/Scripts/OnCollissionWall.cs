4dbaa853abdd5d184854ec24407855e27df288ac
Banerry <43846981+Banerry@users.noreply.github.com>
2019-11-03 13:55:37 +0100

    added terain, added wallcollision

---
+++ NeuralNetworkRaceGame2/Assets/Scripts/OnCollissionWall.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollissionWall : MonoBehaviour
+{
+    public bool detectCollision = false;
+    public GameObject car;
+    //public Vector3 startPosition;
+
+    private Vector3 startPosition;
+    private Quaternion startRotation;
+    private Rigidbody rbCar;
+
+    private void Start()
+    { //filling needed variables
+        startPosition = car.transform.position;
+        startRotation = car.transform.rotation;
+        rbCar = car.GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collision detected");
+        Debug.Log(collision.gameObject.name);
+        if (detectCollision)
+        {
+            if(collision.gameObject.name == "track_walls") //
+            {
+                car.transform.position = startPosition;
+                car.transform.rotation = startRotation;
+                rbCar.velocity = new Vector3(0, 0, 0);
+                
+            }
+        }
+    }
+}
