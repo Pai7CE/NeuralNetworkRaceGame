2db338af6017dcc4735d778c3d8eeddaae760f67
Banerry <43846981+Banerry@users.noreply.github.com>
2019-12-26 11:55:05 +0100

    best time lap works now

--- NeuralNetworkRaceGame2/Assets/Scripts/CanvasSpeed.cs
+++ NeuralNetworkRaceGame2/Assets/Scripts/CanvasSpeed.cs
@@ -1,24 +1,47 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class CanvasSpeed : MonoBehaviour
 {
 
     public Rigidbody target;
-    public Text speedText;
+    public Text speedUI;
+    private string currentSpeed;
 
     // Update is called once per frame
 
     private void Start()
     {
         InvokeRepeating("UpdateSpeed", .2f, .2f);
+        //StartCoroutine(speedUpdate());
     }
+    //private IEnumerator speedUpdate()
+    //{
+    //    while (Application.IsPlaying(target))
+    //    {
+    //        Vector3 prevPos = target.position;
+    //        yield return new WaitForEndOfFrame();
+    //        Vector3 newPos = target.position;
+    //        Vector3 deltaPos = prevPos - newPos;
+    //        double currVel = (deltaPos).magnitude / Time.deltaTime;
+    //        double cutVel = System.Math.Round(currVel, 0);
+    //        currentSpeed = cutVel.ToString();            
+    //        Debug.Log(currVel);
+
+    //    }
+    //}
     private void UpdateSpeed()
     {
         Rigidbody rb = GetComponent<Rigidbody>();
         rb = target;
         double velocity = rb.velocity.magnitude * 3.6;
-        double cutFloat = System.Math.Round(velocity, 2);
-        speedText.text = "Speed: " + cutFloat;
+        double cutFloat = System.Math.Round(velocity, 0);
+
+        speedUI.text =  cutFloat.ToString();
+
+
+
     }
+    
 }
---
+++ NeuralNetworkRaceGame2/Assets/Scripts/LapCompleted.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapCompleted : MonoBehaviour
+{
+
+    public BoxCollider halfway;
+    public BoxCollider finish;
+
+    public GameObject bestMinuteBox;
+    public GameObject bestSecondBox;
+    public GameObject bestMilliBox;
+
+    private float countMilli;
+    private int countSecond;
+    private int countMinute;
+
+
+    private bool halfwayReached;
+    private bool initialLap;
+    void Start()
+    {
+        halfwayReached = false;
+        initialLap = true;
+    }
+    //TODO: delete after game is complete
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            halfwayReached = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other == halfway)
+        {
+            halfwayReached = true;
+        }
+        if(other == finish && halfwayReached == true)
+        {
+            lapCompleted();
+        }
+    }
+
+    private void lapCompleted()
+    {
+        Debug.Log("lap complete");
+
+        if (initialLap)
+        {
+            updateTimer();
+        }
+        else
+        {
+            if(LapTimeManager.MinuteCount <= countMinute)
+            {
+                if(LapTimeManager.MinuteCount < countMinute)
+                {
+                    updateTimer();
+                }
+                else
+                {
+                    if(LapTimeManager.SecondCount <= countSecond)
+                    {
+                        if(LapTimeManager.SecondCount < countSecond)
+                        {
+                            updateTimer();
+                        }
+                        else
+                        {
+                            if(LapTimeManager.MilliCount < countMilli)
+                            {
+                                updateTimer();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+              
+        LapTimeManager.MilliCount = 0;
+        LapTimeManager.SecondCount = 0;
+        LapTimeManager.MinuteCount = 0;
+
+        initialLap = false;
+        halfwayReached = false;
+    }
+    private void updateTimer()
+    {
+        if (LapTimeManager.MilliCount <= 9)
+        {
+            bestMilliBox.GetComponent<Text>().text = "0" + LapTimeManager.MilliCount;
+        }
+        else
+        {
+            bestMilliBox.GetComponent<Text>().text = LapTimeManager.MilliCount + "";
+        }
+        if (LapTimeManager.SecondCount <= 9)
+        {
+            bestSecondBox.GetComponent<Text>().text = "0" + LapTimeManager.SecondCount + ".";
+        }
+        else
+        {
+            bestSecondBox.GetComponent<Text>().text = LapTimeManager.SecondCount + ".";
+        }
+        if (LapTimeManager.MinuteCount <= 9)
+        {
+            bestMinuteBox.GetComponent<Text>().text = "0" + LapTimeManager.MinuteCount + ":";
+        }
+        else
+        {
+            bestMinuteBox.GetComponent<Text>().text = LapTimeManager.MinuteCount + ":";
+        }
+        countMilli = LapTimeManager.MilliCount;
+        countSecond = LapTimeManager.SecondCount;
+        countMinute = LapTimeManager.MinuteCount;
+    }
+}
